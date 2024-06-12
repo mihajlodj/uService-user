cac052c63f8da27c00a4c1206cf1c03d71b0417f
Vuk Vukovic <vuk.vukovic.2000@gmail.com>
2024-06-13 00:31:55 +0200

    feat: prometheus metrics (#27)

---
+++ src/main/java/ftn/userservice/monitoring/WebMetricsConfig.java
@@ -0,0 +1,19 @@
+package ftn.userservice.monitoring;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@AllArgsConstructor
+public class WebMetricsConfig implements WebMvcConfigurer {
+
+    private MetricsInterceptor metricsInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(metricsInterceptor);
+    }
+
+}
